5b81f51661fdca57d4d1c8fd37d19a5299446baa
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-07-20 17:13:06 -0600

    first practice with typing

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello Typescript!");
+var number1 = 9;
+var number2 = 10;
+var add = function (n1, n2) {
+    return n1 + n2;
+};
+var answer = add(number1, number2);
+console.log(answer);
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("Hello Typescript!")
+const number1 = 9
+const number2 = 10
+
+
+const add = (n1: number, n2: number) => {
+    return n1 + n2
+}
+
+
+let answer = add(number1, number2)
+
+console.log(answer)
